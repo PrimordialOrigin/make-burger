bcac76034d36072d9882a4b386d8c2a882477493
Primordial <RamerafheKaone@protonmail.com>
2024-07-21 22:35:58 +0200

    fix:cleaning up the code

--- src/Burger.jsx
+++ src/Burger.jsx
@@ -5,18 +5,24 @@ import './burger.css';
 function Burger() {
   const [burgerIngredients, setBurgerIngredients] = useState([]);
 
+  //finds the filling to be added
+  //generatre a unique key using the date 
   const addIngredient = (id) => {
     const ingredient = allIngredients.find(item => item.id === id);
     setBurgerIngredients([...burgerIngredients, {...ingredient, key: Date.now()}]);
   };
 
+  
   const removeIngredient = (key) => {
     setBurgerIngredients(burgerIngredients.filter(ingredient => ingredient.key !==key));
   };
   
   return (
+
     <div className='container'>
+
       <div className='top-bun'></div>
+
       {burgerIngredients.map((ingredient, index) => (
         <div 
           key={index} 
@@ -26,14 +32,19 @@ function Burger() {
           <p>{ingredient.fillings}</p>
         </div>
       ))}
+
       <div className='bottom-bun'></div>
+
       <div>
         <button onClick={() => addIngredient(1)}>Add Tomato</button>
         <button onClick={() => addIngredient(2)}>Add Meat</button>
         <button onClick={() => addIngredient(3)}>Add Lettuce</button>
       </div>
+    
     </div>
+
   );
+
 }
 
 export default Burger;
